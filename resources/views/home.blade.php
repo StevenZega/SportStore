15dea0e31bf5ac21eeb04196e7f5098e098f7e7b
StevenZega <steven.422024020@civitas.ukrida.ac.id>
2025-06-16 09:23:46 +0700

    Memperbarui tampilan navbar home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,33 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Home</title>
+    <title>Document</title>
     <link rel="stylesheet" href="css/home.css">
 </head>
 
 <body>
     <nav>
-        <div class="logo">SZ-Sport</div>
-        <ul class="nav-links">
-            <li><a href="/">Home</a></li>
-            <li><a href="/product">Product</a></li>
-            <li><a href="/order">Order</a></li>
-        </ul> 
-        <button class="sign-in-button">Sign In
-            <div class="inner-button">
-                <svg id="Arrow" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" class="icon">
-                <defs>
-                    <linearGradient y2="100%" x2="100%" y1="0%" x1="0%" id="iconGradient">
-                    <stop style="stop-color:#FFFFFF;stop-opacity:1" offset="0%"></stop>
-                    <stop style="stop-color:#AAAAAA;stop-opacity:1" offset="100%"></stop>
-                    </linearGradient>
-                </defs>
-                <path fill="url(#iconGradient)" d="M4 15a1 1 0 0 0 1 1h19.586l-4.292 4.292a1 1 0 0 0 1.414 1.414l6-6a.99.99 0 0 0 .292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 0 0-.216-.324l-6-6a1 1 0 0 0-1.414 1.414L24.586 14H5a1 1 0 0 0-1 1z"></path></svg>
-            </div>
-        </button>
+        <div class="nav-left">
+            <h1>SZ-Sport</h1>
+        </div>
+        <div class="nav-center">
+            <ul class="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/product">Product</a></li>
+                <li><a href="/order">Order</a></li>
+            </ul>
+        </div>
+        <div class="nav-right">
+            <button class="sign-in-button">Sign In
+                <svg id="Arrow" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" class="arrow-icon">
+                    <defs>
+                        <linearGradient y2="100%" x2="100%" y1="0%" x1="0%" id="iconGradient"><stop style="stop-color:#FFFFFF;stop-opacity:1" offset="0%"></stop><stop style="stop-color:#AAAAAA;stop-opacity:1" offset="100%"></stop></linearGradient>
+                    </defs>
+                    <path fill="url(#iconGradient)" d="M4 15a1 1 0 0 0 1 1h19.586l-4.292 4.292a1 1 0 0 0 1.414 1.414l6-6a.99.99 0 0 0 .292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 0 0-.216-.324l-6-6a1 1 0 0 0-1.414 1.414L24.586 14H5a1 1 0 0 0-1 1z"></path>
+                </svg>
+            </button>
+        </div>
     </nav>
 
-    <h1>Halaman Home</h1>
+<!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 
+    <div class="container-1">
+
+    </div>
+
+<!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+    <div class="container-2">
+
+    </div>
+
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 </body>
 </html>
